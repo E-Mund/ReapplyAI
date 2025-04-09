b500e473f0af13667009940ee86bcd79aad7161a
E-Mund <ewm58972@uga.edu>
2025-04-09 17:25:20 -0400

    added login button to header

--- reapplyai/src/app/components/Header/Header.tsx
+++ reapplyai/src/app/components/Header/Header.tsx
@@ -1,4 +1,6 @@
+'use client'
 import Image from "next/image";
+
 import Link from "next/link";
 
 import Button from "../Button/Button";
@@ -8,13 +10,16 @@ import "./header.css";
 export default function Header() {
     return (
         <header className="header">
-            <div>
+            <div className="title">
                 <h1>ReapplyAI</h1>
                 <ul>
                     <li><Link href="/">Home</Link></li>
                     <li><Link href="/resumes">Resumes</Link></li>
                     <li><Link href="/templates">Templates</Link></li>
                 </ul>
+            </div>
+            <div className="login">
+                <Button name="Login | Signup" onClick={() => console.log("Clicked!")}/>
             </div>
         </header>
     );
