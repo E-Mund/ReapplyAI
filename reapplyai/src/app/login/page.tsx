54e947016f17808648d8dd9368e2860fcdc38102
ARod <ajrodz2004@gmail.com>
2025-04-13 22:42:00 -0400

    removed duplicate footer component. Moved adding sections part from home page to resumes page

--- reapplyai/src/app/login/page.tsx
+++ reapplyai/src/app/login/page.tsx
@@ -19,8 +19,6 @@ export default function Home() {
 
                 </div>
             </main>
-            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-            </footer>
         </div>
     );
 }
--- reapplyai/src/app/signup/page.tsx
+++ reapplyai/src/app/signup/page.tsx
@@ -9,8 +9,6 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Button name="Create account" onClick={() => console.log("Clicked!")}/>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-      </footer>
     </div>
   );
 }
