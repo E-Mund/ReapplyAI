643f165d9dbbc623e7d5c4686e5205234b42400f
ARod <ajrodz2004@gmail.com>
2025-04-14 17:58:43 -0400

    Repositioned Sections in edit page.tsx

--- reapplyai/src/app/edit/page.tsx
+++ reapplyai/src/app/edit/page.tsx
@@ -1,15 +1,11 @@
-"use client"
-import Image from "next/image";
+"use client";
 
-import Button from "@/components/Button/Button";
 import Sections from "@/components/Section/Sections";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Sections />
-      </main>
+    <div className="flex justify-center">
+      <Sections />
     </div>
   );
 }
