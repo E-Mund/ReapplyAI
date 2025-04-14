91d604839f6f11cbb0435e68984987d83365aee2
ARod <ajrodz2004@gmail.com>
2025-04-14 13:13:35 -0400

    Added Basic Form

--- reapplyai/src/components/Resumes/AddResume.tsx
+++ reapplyai/src/components/Resumes/AddResume.tsx
@@ -1,18 +1,25 @@
 "use client";
 
+import ClipArt from "@/assets/resume-clip-art.png";
+import Image from "next/image";
+
 const AddResume = () => {
+
   return (
     <div className="flex items-center justify-center">
       <div>
-        <h1>+</h1>
+        <Image src={ClipArt} alt="Description of image" width="200"/>
       </div>
-      <div>
-        <h1>Create Resume</h1>
-        <p>Content Here: 
-          _________
-          _________
-        </p>
-      </div>
+      <form className="flex flex-col">
+        <h1>Create Resume:</h1>
+        <input 
+          name="title" 
+          type="text" 
+          placeholder="Title" 
+          required />
+        <textarea name="description" placeholder="Description" required />
+        <button type="submit" className="bg-blue-300 rounded hover:bg-blue-500 ">Add Resume</button>
+      </form>
     </div>
   );
 };
