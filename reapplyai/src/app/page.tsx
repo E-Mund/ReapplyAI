5557644a3b713dcf557d2ddcaa08ced9cf087bba
Avash Ojha <avashojha@Avashs-MacBook-Air.local>
2025-04-17 08:20:23 -0400

    created mongodb files

--- reapplyai/src/app/page.tsx
+++ reapplyai/src/app/page.tsx
@@ -2,9 +2,11 @@
 
 import Welcome from "@/components/Splash/Welcome";
 import Features from "@/components/Splash/Features";
+import connectMongoDB from "../../config/mongodb";
 
 
 export default function Home() {
+  connectMongoDB();
   return (
     <div className="min-h-screen bg-white text-gray-900">
       <Welcome/>
